7784d5e6714e28518c0d28a6c99fd626d5257e62
NTTH7419 <98406395+NTTH7419@users.noreply.github.com>
2024-06-18 15:04:43 +0700

    Complete Experiment promgram

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 # include "program.h"
 
+/*
+	Command 1: Run a sorting algorithm on the given input data.
+		[Execution file] -a [Algorithm] [Input file] [Output parameter(s)]
+		main.exe -a radix-sort input.txt -both
+
+	Command 2: Run a sorting algorithm on the data generated automatically with specified size and order.
+		[Execution file] -a [Algorithm] [Input size] [Input order] [Output parameter(s)]
+		main.exe -a selection-sort 50 -rand -time
+
+	Command 3: Run a sorting algorithm on ALL data arrangements of a specified size.
+		[Execution file] -a [Algorithm] [Input size] [Output parameter(s)]
+		main.exe -a binary-insertion-sort 70000 -comp
+
+	Command 4: Run two sorting algorithms on the given input.
+		[Execution file] -c [Algorithm 1] [Algorithm 2] [Input file]
+		main.exe -c heap-sort merge-sort input.txt
+
+	Command 5: Run two sorting algorithms on the data generated automatically.
+		[Execution file] -c [Algorithm 1] [Algorithm 2] [Input size] [Input order]
+		main.exe -c quick-sort merge-sort 100000 -nsorted
+*/
 
 int main(int argc, char** argv) {
-	ProgramArguments prog_args = processArguments(argc, argv);
-	runProgram(prog_args);
-
-	// unsigned long long comp_count = 0;
-	// int n;
-	// cin >> n;
-	// int *arr = new int[n];
-	// GenerateData(arr, n, 0);
-	// vector<type> v(arr, arr + n);
-	// radixSort(v, comp_count);
-	// cout << comp_count;
+	string sort_name = argv[1];
+	experimentMode(sort_name);
 	return 0;
 }
--- program.h
+++ program.h
@@ -10,42 +10,15 @@
 
 using namespace std;
 
-#define DEFAULT 0
-
-#define ALGORITHM_MODE 101
-#define COMPARISON_MODE 102
-
-#define INP_FILE 103
-#define INP_GENERATED 104
-
 #define ORDER_RANDOM 1 << 1
 #define ORDER_NSORTED 1 << 2
 #define ORDER_SORTED 1 << 3
 #define ORDER_REVERSE 1 << 4
-#define ORDER_ALL 	ORDER_RANDOM | ORDER_NSORTED | ORDER_SORTED | ORDER_REVERSE
 
-#define OUT_TIME 1 << 5
-#define OUT_COMPARISON 1 << 6
-#define OUT_BOTH 	OUT_TIME | OUT_COMPARISON
+typedef void (*p_func1)(vector<type>&, ull&);
+typedef void (*p_func2)(vector<type>&);
 
-typedef void (*p_func)(vector<type>&, ull&);
 typedef int ArgValue;
 
-struct ProgramArguments{
-	ArgValue mode = DEFAULT;
-
-	ArgValue input_mode = DEFAULT;
-	int input_size = 0;
-	ArgValue input_order = ORDER_ALL;
-	string input_file = "";
-
-	ArgValue output_para = DEFAULT;
-
-	int n_sort_algos = 0;
-	p_func sort_funcs[2] = {nullptr};
-	string algo_name[2] = {""};
-};
-
-ProgramArguments processArguments(int argc, char** argv);
-void runProgram(ProgramArguments prog_args);
+void experimentMode(string sort_name);
 
